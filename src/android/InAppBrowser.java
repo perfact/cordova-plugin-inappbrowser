23d4014c3533cc4311eaf8c1032bba6d7ea3c539
Ján Jockusch <jan.jockusch@perfact.de>
2025-02-18 11:38:08 +0100

    Add more file types

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1194,7 +1194,7 @@ public class InAppBrowser extends CordovaPlugin {
             String url = request.getUrl().toString();
 
             LOG.d(LOG_TAG, "shouldOverrideUrlLoading entered! " + url);
-            if (url.matches(".*\\.(pdf|doc|docx|xls|xlsx|ppt|pptx|zip|rar|mp3|mp4|avi|mov|apk|odt|ods)(\\?.*)?$")) {
+            if (url.matches(".*\\.(pdf|rtf|doc|docx|xls|xlsx|ppt|pptx|zip|rar|mp3|mp4|mpg|mpeg|avi|wmv|mov|apk|odt|ods|txt|csv)(\\?.*)?$")) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setDataAndType(Uri.parse(url), "*/*");
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
