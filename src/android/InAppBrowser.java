b8f223ba2b9a6abf40c82d2c5419bce901558e07
Ján Jockusch <jan.jockusch@perfact.de>
2025-02-18 12:28:25 +0100

    Fix typo

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1194,7 +1194,7 @@ public class InAppBrowser extends CordovaPlugin {
 
             if (url.matches(".*\\.(pdf|rtf|doc|docx|xls|xlsx|ppt|pptx|zip|rar|mp3|mp4|mpg|mpeg|avi|wmv|mov|apk|odt|ods|txt|csv)(\\?.*)?$")) {
                 Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setDataAndType(Uri.parse(url));
+                intent.setData(Uri.parse(url));
                 cordova.getActivity().startActivity(intent);
                 return true;
             }
